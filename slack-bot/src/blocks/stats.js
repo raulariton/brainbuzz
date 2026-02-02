89a8debae283034451fb03937c1a43567fe3dce3
raul-it0o0 <ariton.raul@gmail.com>
2026-02-02 20:34:21 +0200

    Improve output in `/stats` command

--- slack-bot/src/blocks/stats.js
+++ slack-bot/src/blocks/stats.js
@@ -37,11 +37,11 @@ export default async function getStatsBlocks(name, scope, id) {
 
         const lines = [
             `${name}, you have:`,
-            `- *Participated in ${stats.total_quizzes} quiz(zes)*`,
-            `- *Won ${stats.total_wins} times*${stats.total_wins > 0 ? '' : ' (I believe in you! :grin:)'}`,
-            `- *A ${(stats.win_rate * 100).toFixed(2)}% win rate*${stats.win_rate > 75 ? ' (Nice! :star-struck)' : ''}`,
-            `- *Placed ${stats.total_top_3_finishes} times in the top 3 of a quiz.*`,
-            `- *Answered ${stats.total_correct_answers} questions correctly.*${stats.correctAnswers >= 0.8 * stats.total_quizzes ? ' (You know-it-all! :nerd-face:)' : ''}`
+            `:clipboard: *Participated in ${stats.total_quizzes} quiz(zes)*`,
+            `:trophy: *Won ${stats.total_wins} times*${stats.total_wins > 0 ? '' : ' (I believe in you! :grin:)'}`,
+            `:dart: *A ${(stats.win_rate * 100).toFixed(2)}% win rate*${stats.win_rate > 75 ? ' (Nice! :star-struck)' : ''}`,
+            `:medal: *Placed ${stats.total_top_3_finishes} times in the top 3 of a quiz.*`,
+            `:white_check_mark: *Answered ${stats.total_correct_answers} questions correctly.*${stats.correctAnswers >= 0.8 * stats.total_quizzes ? ' (You know-it-all! :nerd-face:)' : ''}`
         ];
 
         return [
@@ -60,12 +60,12 @@ export default async function getStatsBlocks(name, scope, id) {
         
     }
         const lines = [
-            `${name} team, you guys have:`,
-            `- *Hosted a total of ${stats.total_quizzes} quiz(zes)*`,
-            `- *Average score across all quizzes:* ${(stats.average_score * 100).toFixed(2)}%${stats.average_score > 70 ? ' (Great job! :trophy:)' : ''}`,
-            `- *Average participation rate:* ${(stats.average_participation * 100).toFixed(2)}%${stats.average_participation > 50 ? ' (Nice work! :clap:)' : ''}`,
-            `- *Most popular quiz type:* ${stats.most_popular_quiz_type}`,
-            `- *The know-it-all:* <@${stats.highest_scoring_user.user_id}> with ${stats.highest_scoring_user.total_wins} wins and a win rate of ${(stats.highest_scoring_user.win_rate * 100).toFixed(2)}%`
+            `${name === 'Hey' ? `${name}` : `*${name}*`} team, you guys have:`,
+            `:microphone: *Hosted a total of ${stats.total_quizzes} quiz(zes)*`,
+            `:space_invader: *Average score across all quizzes:* ${(stats.average_score * 100).toFixed(2)}%${stats.average_score > 70 ? ' (Great job! :trophy:)' : ''}`,
+            `:busts_in_silhouette: *Average participation rate:* ${(stats.average_participation * 100).toFixed(2)}%${stats.average_participation > 50 ? ' (Nice work! :clap:)' : ''}`,
+            `:fire: *Most popular quiz type:* ${stats.most_popular_quiz_type}`,
+            `:crown: *The know-it-all:* <@${stats.highest_scoring_user.user_id}> with ${stats.highest_scoring_user.total_wins} wins and a win rate of ${(stats.highest_scoring_user.win_rate * 100).toFixed(2)}%`
         ];
 
         return [
